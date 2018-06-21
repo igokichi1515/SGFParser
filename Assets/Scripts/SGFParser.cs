e54ab5592d4360e3a05cd392673356a650399d04
intenseG <intenseg1515@gmail.com>
2018-06-22 00:36:18 +0900

    SGFParser.csに処理を追加

--- Assets/Scripts/SGFParser.cs
+++ Assets/Scripts/SGFParser.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
 using UnityEngine;
 
 public class SGFParser : MonoBehaviour
 {
 
+    private readonly int boardSize = 13;        //碁盤のサイズ
     public List<TextAsset> sgfFiles;            //SGFファイル（txt形式）のリスト
     private List<List<int>> sgfIndexList;       //最後のログ出力用の変換後データ格納リスト
     private List<int> treeNodes;                //分岐が終わった or 無い手順のときに手順情報を保持するリスト
     private List<List<int>> branchIndexList;    //分岐があったときに一時的に手順情報を保持するリスト
-    private List<int> treeBranchList;           //分岐があったときのtreeNodesより後の手順情報を保持するリスト
-    private string sgfStr = "";                 //SGFファイルから読み込んだテキストデータ
-
+    private List<int> treeBranchList;           //分岐があったときにtreeNodesより後の手順情報を保持するリスト
+    private string sgfStr;                      //SGFファイルから読み込んだテキストデータ
     private bool isTree;                        //現在、分岐があるかどうかのフラグ　分岐あり -> true, 分岐無し -> false
-    private int treeDepth = 0;                  //分岐の深さ
+    private bool isRight;                       //現在の行に正解手順コメント「C[RIGHT]」が含まれているかどうか
+    private int treeDepth;                      //分岐の深さ
+
+    private List<int> initPlaceListBlack;       //初期配石のリスト（黒）
+    private List<int> initPlaceListWhite;       //初期配石のリスト（白）
 
     void Start()
     {
@@ -32,9 +37,12 @@ public class SGFParser : MonoBehaviour
     {
         foreach (TextAsset sgf in sgfFiles)
         {
+            Debug.Log("ファイル名: " + sgf.name);
+
             //変数の初期化
             treeDepth = 0;
             isTree = false;
+            isRight = false;
             treeNodes = new List<int>();
             treeBranchList = new List<int>();
             branchIndexList = new List<List<int>>();
@@ -43,7 +51,9 @@ public class SGFParser : MonoBehaviour
             StringBuilder sb = new StringBuilder(sgf.text);
             sb.Replace("SZ[13]", "")
             .Replace("MULTIGOGM[1]", "")
-            .Replace("AP[MultiGo:4.2.4]", "");
+            .Replace("AP[MultiGo:4.2.4]", "")
+            .Replace("\t", "")
+            .Replace(" ", "");
 
             //ファイルのテキストを行ごとのテキストデータにして配列に格納
             sgfStr = sb.ToString();
@@ -52,11 +62,29 @@ public class SGFParser : MonoBehaviour
             //行ごとにループ
             for (int n = 0; n < sgfLines.Length; n++)
             {
-                if (sgfLines[n].Length < 7) continue;
-                if (n == 0 || n == 1) continue;
-                
+                //初期配石データが含まれている場合は解析してリストに格納する
+                if (sgfLines[n].Contains("AB["))
+                {
+                    //黒
+                    ParseInitPlacement(sgfLines[n], true);
+                }
+                else if (sgfLines[n].Contains("AW["))
+                {
+                    //白
+                    ParseInitPlacement(sgfLines[n], false);
+                }
+
+                // if (!sgfLines[n].Contains(";B[") || !sgfLines[n].Contains(";W[")) continue;
+                // if (n == 0 || n == 1) continue;
+
                 Debug.Log("文字列(1行): " + sgfLines[n]);
                 Debug.Log("ツリーの深さ: " + treeDepth);
+
+                //正解手順コメントがあるかどうか判定してフラグを切り替える
+                if (sgfLines[n].Contains("C[RIGHT]"))
+                {
+                    isRight = true;
+                }
 
                 //1行に分岐終了の括弧がいくつあるか
                 int endCount = CountChar(sgfLines[n], ')');
@@ -67,7 +95,7 @@ public class SGFParser : MonoBehaviour
                     //分岐の深さを1加算
                     treeDepth++;
                     if (treeDepth >= 1) isTree = true;
-                    
+
                     //座標データごとに分割して配列に格納
                     string[] nodeArray = sgfLines[n].Split(';');
                     foreach (string node in nodeArray)
@@ -75,7 +103,7 @@ public class SGFParser : MonoBehaviour
                         //座標データを囲む大括弧がどの位置からはじまってどの位置で終わるか
                         int open = node.IndexOf("[");
                         int close = node.IndexOf("]");
-                        
+
                         //座標データじゃなければスキップ
                         if (open < 0 || close < 0)
                         {
@@ -153,11 +181,31 @@ public class SGFParser : MonoBehaviour
                                 {
                                     treeNodes.Add(bIndex);
                                 }
+
+                                foreach (int node in treeNodes)
+                                {
+                                    Debug.Log("追加したノード -> " + node);
+                                }
                             }
                         }
                     }
                     else
                     {
+                        foreach (int node in treeNodes)
+                        {
+                            Debug.Log("追加したノード -> " + node);
+                        }
+                                                
+                        //正解手順だったら999、失敗手順なら998を手順の最後に追加
+                        if (isRight)
+                        {
+                            treeNodes.Add(999);
+                        }
+                        else
+                        {
+                            treeNodes.Add(998);
+                        }
+
                         sgfIndexList.Add(treeNodes);
                         treeNodes = new List<int>();
                         treeBranchList = new List<int>();
@@ -224,6 +272,10 @@ public class SGFParser : MonoBehaviour
                                     treeNodes.Add(bIndex);
                                 }
                             }
+                            foreach (int node in treeNodes)
+                            {
+                                Debug.Log("追加したノード -> " + node);
+                            }
                         }
                     }
                     else
@@ -244,7 +296,22 @@ public class SGFParser : MonoBehaviour
                                         treeNodes.Add(bIndex);
                                     }
                                 }
+                                
+                                foreach (int node in treeNodes)
+                                {
+                                    Debug.Log("追加したノード -> " + node);
+                                }                        
                             }
+                        }
+
+                        //正解手順だったら999、失敗手順なら998を手順の最後に追加
+                        if (isRight)
+                        {
+                            treeNodes.Add(999);
+                        }
+                        else
+                        {
+                            treeNodes.Add(998);
                         }
 
                         sgfIndexList.Add(treeNodes);
@@ -255,11 +322,90 @@ public class SGFParser : MonoBehaviour
                 }
             }
 
-            //変換結果をログ出力
-            foreach (List<int> indexList in sgfIndexList)
+            //初期配石リストを初期化
+            string[] resultPlaceArray = new string[boardSize * boardSize];
+            for (int m = 0; m < boardSize * boardSize; m++)
             {
-                string result = string.Join(",", indexList.Select(x => x.ToString()).ToArray());
+                resultPlaceArray[m] = "0";
+            }
+            for (int k = 0; k < initPlaceListBlack.Count; k++)
+            {
+                int index = initPlaceListBlack[k];
+                resultPlaceArray[index] = "1";
+            }
+            for (int l = 0; l < initPlaceListWhite.Count; l++)
+            {
+                int index = initPlaceListWhite[l];
+                resultPlaceArray[index] = "2";
+            }
+
+            //変換結果をログ出力（初期配石）
+            string placeResult = string.Join(",", resultPlaceArray);
+            Debug.Log("\"" + placeResult + "\"");
+
+            //変換結果をログ出力（手順）
+            string result = "";
+            for (int j = 0; j < sgfIndexList.Count; j++)
+            {
+                foreach (int nube in sgfIndexList[j])
+                {
+                    Debug.Log("デバッグ用" + j + "-> " +  nube);
+                }
+                
+                result = "";
+                result += "\"" + string.Join(",", sgfIndexList[j].Select(x => x.ToString()).ToArray()) + "\"";
+
+                if (j < sgfIndexList.Count)
+                {
+                    result += "," + "\n";
+                }
+                else
+                {
+                    result += "\n";
+                }
+
+                //変換結果をコンソールで表示
                 Debug.Log(result);
+            }
+
+            //初期配石リストを初期化
+            initPlaceListBlack = new List<int>();
+            initPlaceListWhite = new List<int>();
+        }
+    }
+
+    //初期配石データを解析
+    private void ParseInitPlacement(string nodeStr, bool isBlack)
+    {
+        //初期配石リストを初期化
+        initPlaceListBlack = new List<int>();
+        initPlaceListWhite = new List<int>();        
+
+        string target = "[[a-z]*]";
+        if (isBlack)
+        {
+            if (Regex.IsMatch(nodeStr, target))
+            {
+                MatchCollection mc = Regex.Matches(nodeStr, target);
+                foreach (Match m in mc)
+                {
+                    string node = m.Value.Substring(1, 2);
+                    int index = ConvertZahyoStringToInt(node);
+                    initPlaceListBlack.Add(index);
+                }
+            }
+        }
+        else
+        {
+            if (Regex.IsMatch(nodeStr, target))
+            {
+                MatchCollection mc = Regex.Matches(nodeStr, target);
+                foreach (Match m in mc)
+                {
+                    string node = m.Value.Substring(1, 2);
+                    int index = ConvertZahyoStringToInt(node);
+                    initPlaceListWhite.Add(index);
+                }
             }
         }
     }
@@ -267,8 +413,6 @@ public class SGFParser : MonoBehaviour
     //SGFの文字座標を数字座標に変換
     private int ConvertZahyoStringToInt(string zahyo)
     {
-        int boardSize = 13; //碁盤のサイズ
-
         int zahyo1 = CharToInt(zahyo[0]);
         int zahyo2 = CharToInt(zahyo[1]);
         int index = zahyo1 + (zahyo2 * boardSize);
